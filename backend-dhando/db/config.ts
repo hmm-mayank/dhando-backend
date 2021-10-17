449ef66318d3ccb5120eb3409e644b8a35de5c36
mayank-addepar <88153552+mayank-addepar@users.noreply.github.com>
2021-10-17 09:43:53 +0530

    status chafixnge

--- backend-dhando/db/config.ts
+++ backend-dhando/db/config.ts
@@ -5,7 +5,7 @@ const dbName =   'dhando'  //process.env.DB_NAME as string
 const dbUser =   'postgres' //process.env.DB_USER as string
 const dbHost = 'localhost' //process.env.DB_HOST
 const dbDriver =  'postgres' //process.env.DB_DRIVER as Dialect
-const dbPassword =  'qwerty'//process.env.DB_PASSWORD
+const dbPassword =  'myPassword'//process.env.DB_PASSWORD
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
     host: dbHost,
